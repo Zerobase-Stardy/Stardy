b72524aec086cd4116762a5f59c2f11625e11986
h0d00 <110148272+h0d00@users.noreply.github.com>
2022-08-15 20:54:12 +0900

    Header, Footer 레이아웃  (#13)
    * feat[footer complete]
    
    * Header 디자인 적용
    
    * [fix]:mylecture banner add
    
    Co-authored-by: DESKTOP-JTDD7A9\vheh0 <vheh0901@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,37 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import Footer from "./Layout/Footer";
+import Header from "./Layout/Header";
+import Community from "./pages/Community";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import MyLecture from "./pages/MyLecture";
+import Mypage from "./pages/Mypage";
+import ProGamer from "./pages/ProGamer";
+import Races from "./pages/Races";
+import GlobalStyles from "./styles/GlobalStyles";
 
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    
+<BrowserRouter>
+<ScrollToTop />  {/* 페이지 이동시 스크롤 위로 고정하는 컴포넌트 */}
+<GlobalStyles />
+<Header />
+<Routes>
+<Route path={"/"} element={<Home />}></Route>
+  <Route path={"/progamer"} element={<ProGamer />}></Route>
+  <Route path={"/races"} element={<Races />}></Route>
+  <Route path={"/community"} element={<Community />}></Route>
+  <Route path={"/mylecture"} element={<MyLecture />}></Route>
+  <Route path={"/login"} element={<Login />}></Route>
+  <Route path={"/mypage"} element={<Mypage />}></Route>
+</Routes>
+<Footer />
+</BrowserRouter>
+
   );
 }
 
--- frontend/src/Layout/Header.js
+++ frontend/src/Layout/Header.js
@@ -1,7 +1,59 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from "react-router-dom";
 
-export default function Header() {
-  return (
-    <div>Header</div>
+
+const LinkItem = ({ active, children, to }) => (
+  <Link to = {to} className={`menu-item ${active ? active : ""}`}> { children } </Link>
+  );
+  
+  
+  export default function Header() {
+    return (
+      <Wrap>
+        <LogoArea> 
+          <LinkItem to="/"> STARdy </LinkItem> 
+        </LogoArea>
+        <UserNaviArea > 
+          <LinkItem to="/login"> Login </LinkItem> | <LinkItem to="/mypage"> My Page </LinkItem> 
+        </UserNaviArea>
+        <HeaderNav>
+          <LinkItem to="/progamer">Pro-gamer</LinkItem>
+          <LinkItem to="/races">Races</LinkItem>
+          <LinkItem to="/community">Community</LinkItem>
+          <LinkItem to="/mylecture">내 강의실</LinkItem>
+          <input />
+        </HeaderNav>
+    </Wrap>
   )
 }
+
+
+
+const Wrap = styled.div`
+
+background-color: black;
+color: white;
+position: relative;
+`
+const LogoArea = styled.div`
+  width: 100%;
+  font-size: 32px;
+  text-align: center;
+  font-weight: bold;
+  color: white;
+  margin-bottom: 8px;
+`
+const HeaderNav = styled.div`
+  padding: 4px;
+  display: flex;
+  justify-content: space-between;
+`
+
+const UserNaviArea = styled.div`
+  position: absolute;
+  right: 4px;
+  top: 12px;
+  font-size: 14px;
+
+`
