fee4d05fb6ef61ce4dd09bb754239010550328b6
rho510 <rho510@naver.com>
2022-08-20 01:41:04 +0900

    [feat]게시판 구현,글쓰기 에디터 라이브러리 적용

--- frontend/src/Layout/Header.js
+++ frontend/src/Layout/Header.js
@@ -1,53 +1,47 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-
-
 const LinkItem = ({ active, children, to }) => (
-  <Link to = {to} className={`menu-item ${active ? active : ""}`}> { children } </Link>
+  <Link to={to} className={`menu-item ${active ? active : ""}`}>
+    {" "}
+    {children}{" "}
+  </Link>
+);
+
+export default function Header(props) {
+  return (
+    <HeaderArea>
+      <Wrap>
+        <LogoArea>
+          <LinkItem to="/">STARDY</LinkItem>
+        </LogoArea>
+        <UserNaviArea>
+          <Login onClick={props.toggle}> Login </Login> |
+          <LinkItem to="/mypage"> My Page </LinkItem>
+        </UserNaviArea>
+        <HeaderNav>
+          <LinkItem to="/progamer">Pro-gamer</LinkItem>
+          <LinkItem to="/races">Races</LinkItem>
+          <LinkItem to="/post">Community</LinkItem>
+          <LinkItem to="/mylecture">내 강의실</LinkItem>
+          <input />
+        </HeaderNav>
+      </Wrap>
+    </HeaderArea>
   );
-  
-
-  export default function Header(props) {
-
-
-    return (
-      <HeaderArea>
-          <Wrap>
-            <LogoArea> 
-              <LinkItem to="/">
-                STARDY 
-              </LinkItem> 
-            </LogoArea>
-            <UserNaviArea > 
-              <Login onClick={props.toggle}>  Login </Login> | <LinkItem to="/mypage"> My Page </LinkItem> 
-            </UserNaviArea>
-            <HeaderNav>
-              <LinkItem to="/progamer">Pro-gamer</LinkItem>
-              <LinkItem to="/races">Races</LinkItem>
-              <LinkItem to="/community">Community</LinkItem>
-              <LinkItem to="/mylecture">내 강의실</LinkItem>
-              <input />
-            </HeaderNav>
-        </Wrap>
-      </HeaderArea>
-  )
 }
 
-
-
 const Wrap = styled.div`
-width: 80%;
-margin: 0 auto;
-position: relative;
-`
+  width: 80%;
+  margin: 0 auto;
+  position: relative;
+`;
 
 const HeaderArea = styled.header`
-background-color: black;
-color: white;
-
-`
+  background-color: black;
+  color: white;
+`;
 const LogoArea = styled.div`
   width: 100%;
   font-size: 32px;
@@ -56,27 +50,26 @@ const LogoArea = styled.div`
   color: white;
   margin-bottom: 8px;
   font-family: Gowun Dodum;
-`
+`;
 const HeaderNav = styled.div`
   padding: 4px;
   display: flex;
   justify-content: space-between;
-`
+`;
 
 const UserNaviArea = styled.div`
   position: absolute;
   right: 4px;
   top: 12px;
   font-size: 14px;
-
-`
+`;
 
 const Login = styled.button`
-border: 0;
-outline: 0;
-background-color: transparent;
-color: white;
-cursor: pointer;
-font-family: 'Galmuri';
-font-size: 14px;
-`
+  border: 0;
+  outline: 0;
+  background-color: transparent;
+  color: white;
+  cursor: pointer;
+  font-family: "Galmuri";
+  font-size: 14px;
+`;
