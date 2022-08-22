8fc0d87f8adc0a696aa37d0f85e840e814ffaaac
Dohun Lee <nestour95@gmail.com>
2022-08-22 15:08:00 +0900

    [feat] 출석 조회 API 추가
    what : 출석 조회 API  추가
    why : 기간으로 로그인한 회원의 출석을 가져오는 API 작성

--- Backend/src/main/java/com/github/backend/service/AttendanceService.java
+++ Backend/src/main/java/com/github/backend/service/AttendanceService.java
@@ -1,8 +1,14 @@
 package com.github.backend.service;
 
+import com.github.backend.model.dto.AttendanceDto;
+import com.github.backend.model.dto.AttendanceDto.GetRequest;
+import java.util.List;
+
 public interface AttendanceService {
 
 	void checkTodayAttendance(String email);
+
+	List<AttendanceDto.Info> getAttendances(GetRequest request);
 
 
 }
