8e143406ea60a9773504fe6f2bf26ff02f720594
Dohun Lee <nestour95@gmail.com>
2022-08-24 18:14:38 +0900

    [refactor] 기존 MemberCourseService의 기능 분리
    what : 강의 해금 기능을 MemberCourseService에서 분리하여 따로 생성했습니다. 더불어 필요한 부분의 코드를 수정했습니다.

--- Backend/src/main/java/com/github/backend/service/memberCourse/MemberCourseService.java
+++ Backend/src/main/java/com/github/backend/service/memberCourse/MemberCourseService.java
@@ -2,7 +2,7 @@ package com.github.backend.service.memberCourse;
 
 public interface MemberCourseService {
 
-	void takeCourse(String email, Long courseId);
-
 	void toggleBookmark(String email, Long memberCourseId);
+
+
 }
