d24031b0739f216bd95755ee90487ad7138037c0
Dohun Lee <nestour95@gmail.com>
2022-08-24 12:34:14 +0900

    [fix]  MemberException 수정
    what : RuntimeException 상속하도록 수정
    why : 예외를 던지기위해서는 RuntimeException이 필요하기 때문에 수정합니다.

--- Backend/src/main/java/com/github/backend/exception/MemberException.java
+++ Backend/src/main/java/com/github/backend/exception/MemberException.java
@@ -9,12 +9,21 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 
-public class MemberException {
+public class MemberException extends RuntimeException{
     private MemberErrorCode errorCode;
     private String errorMessage;
 
     public MemberException(MemberErrorCode errorCode){
+        super(errorCode.getDescription());
         this.errorCode = errorCode;
         this.errorMessage = errorCode.getDescription();
     }
+
+    public MemberException(MemberErrorCode errorCode,Throwable cause) {
+        super(errorCode.getDescription(),cause);
+        this.errorCode = errorCode;
+        this.errorMessage = getErrorMessage();
+    }
+
+
 }
