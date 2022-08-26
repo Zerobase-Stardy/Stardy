cc2e8c6f8a694dbd8d919b7da0bc2f11faf2aac6
Dohun Lee <nestour95@gmail.com>
2022-08-27 03:42:22 +0900

    [build] Swagger 추가 (#102)

--- Backend/src/main/java/com/github/backend/dto/common/MemberInfo.java
+++ Backend/src/main/java/com/github/backend/dto/common/MemberInfo.java
@@ -14,6 +14,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import springfox.documentation.annotations.ApiIgnore;
 
 @AllArgsConstructor
 @NoArgsConstructor
--- Backend/src/main/java/com/github/backend/web/member/controller/AttendanceController.java
+++ Backend/src/main/java/com/github/backend/web/member/controller/AttendanceController.java
@@ -5,6 +5,8 @@ import com.github.backend.dto.attendance.AttendanceDto.Info;
 import com.github.backend.dto.common.MemberInfo;
 import com.github.backend.dto.common.Result;
 import com.github.backend.service.attendance.AttendanceService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,14 +22,23 @@ public class AttendanceController {
 
 	private final AttendanceService attendanceService;
 
+	@ApiOperation(
+		value = "출석 체크하기",
+		notes = "출석 체크 시 50 포인트 추가 , 출석체크는 하루에 한번만 가능 , 로그인 필요"
+	)
+
 	@PostMapping("/members/me/attendances/daily")
-	public ResponseEntity<Result<?>> checkDailyAttendance(@AuthenticationPrincipal MemberInfo memberInfo) {
+	public ResponseEntity<Result<?>> checkDailyAttendance(@ApiIgnore @AuthenticationPrincipal MemberInfo memberInfo) {
 		attendanceService.checkTodayAttendance(memberInfo.getEmail());
 		return ResponseEntity.ok().body(new Result<>(200,true,""));
 	}
 
+	@ApiOperation(
+		value = "출석 조회",
+		notes = "원하는 기간만큼의 출석 조회 가능"
+	)
 	@GetMapping("/members/me/attendances")
-	public ResponseEntity<Result<?>> getAttendances(@AuthenticationPrincipal MemberInfo memberInfo,
+	public ResponseEntity<Result<?>> getAttendances(@ApiIgnore @AuthenticationPrincipal MemberInfo memberInfo,
 		AttendanceDto.GetRequest request) {
 
 		request.setMemberId(memberInfo.getId());
