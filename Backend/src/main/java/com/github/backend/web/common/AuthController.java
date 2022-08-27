bb83d698dce1c12ce1ae65093073b060eeed4f11
Dohun Lee <nestour95@gmail.com>
2022-08-27 22:05:38 +0900

    [feat] 토큰 재발급 API 추가 (#110)
    what : Refresh Token을 이용한 토큰 재발급 API 작성
    why : Access Token 만료 시, Refresh Token으로 재발급 받기 위함

--- Backend/src/main/java/com/github/backend/web/common/AuthController.java
+++ Backend/src/main/java/com/github/backend/web/common/AuthController.java
@@ -3,18 +3,25 @@ package com.github.backend.web.common;
 import com.github.backend.dto.common.MemberInfo;
 import com.github.backend.dto.common.Result;
 import com.github.backend.persist.common.repository.RefreshTokenRepository;
+import com.github.backend.security.jwt.JwtInfo;
+import com.github.backend.security.jwt.TokenService;
+import com.github.backend.security.jwt.Tokens;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
-public class TokenController {
-   final RefreshTokenRepository refreshTokenRepository;
+public class AuthController {
+   private final RefreshTokenRepository refreshTokenRepository;
+   private final TokenService tokenService;
 
     @GetMapping("/logout")
     public ResponseEntity<Result<?>> logout(@AuthenticationPrincipal MemberInfo memberInfo){
@@ -28,4 +35,20 @@ public class TokenController {
                         .build()
         );
     }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<Result<?>> reissueToken(@RequestBody String refreshToken){
+
+        Tokens token = tokenService.refresh(refreshToken);
+
+        return ResponseEntity.ok().body(
+                Result.builder()
+                        .status(200)
+                        .success(true)
+                        .data(token)
+                        .build()
+        );
+    }
+
+
 }
