36b671c2935181ef51044c6350ee8a3e12df912c
Dohun Lee <nestour95@gmail.com>
2022-08-27 18:18:14 +0900

    [feat] Swagger에 JWT 인증 도입

--- Backend/src/main/java/com/github/backend/config/SwaggerConfig.java
+++ Backend/src/main/java/com/github/backend/config/SwaggerConfig.java
@@ -1,12 +1,18 @@
 package com.github.backend.config;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
@@ -16,9 +22,11 @@ public class SwaggerConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.OAS_30)
 			.useDefaultResponseMessages(true)
+			.securityContexts(Arrays.asList(securityContext()))
+			.securitySchemes(Arrays.asList(apiKey()))
 			.apiInfo(apiInfo())
 			.select()
-			.apis(RequestHandlerSelectors.basePackage("com.github.backend.web"))
+			.apis(RequestHandlerSelectors.basePackage("com.github.backend"))
 			.paths(PathSelectors.any())
 			.build();
 	}
@@ -29,5 +37,20 @@ public class SwaggerConfig {
 			.description("enjoy APIs!")
 			.version("0.1")
 			.build();
+	}
+
+	private ApiKey apiKey() {
+		return new ApiKey("Authorization", "Authorization", "header");
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
 	}
 }
