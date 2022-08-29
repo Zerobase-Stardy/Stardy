a90aa465ec645749149eafae8763821c0a946ddb
Dohun Lee <nestour95@gmail.com>
2022-08-30 00:39:07 +0900

    [fix] 불필요한 MemberService 삭제

--- Backend/src/main/java/com/github/backend/service/member/MemberService.java
+++
@@ -1,8 +0,0 @@
-package com.github.backend.service.member;
-
-import com.github.backend.persist.member.Member;
-
-public interface MemberService {
-    Member getMember(String email);
-    Member getMember(Long memberId);
-}
