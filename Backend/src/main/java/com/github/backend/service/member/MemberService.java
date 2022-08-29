5ee26fc2ab941eb5d8bfd4c4c69d4d615b4e8343
Dohun Lee <nestour95@gmail.com>
2022-08-30 00:46:20 +0900

    [fix] MemberInfoManagementService를 MemberService로 변경
    why : 서비스가 크지 않은 이상 분리하지 않는 것이 좋을 것 같아서 변경하였습니다.

--- Backend/src/main/java/com/github/backend/service/member/MemberService.java
+++ Backend/src/main/java/com/github/backend/service/member/MemberService.java
@@ -1,10 +1,7 @@
 package com.github.backend.service.member;
 
-public interface MemberInfoManagementService {
-
-
+public interface MemberService {
 	void editNickname(String memberEmail, String newNickname);
-
 	void withdrawal(String memberEmail);
 
 
--- Backend/src/main/java/com/github/backend/service/member/impl/MemberServiceImpl.java
+++ Backend/src/main/java/com/github/backend/service/member/impl/MemberServiceImpl.java
@@ -1,34 +1,39 @@
 package com.github.backend.service.member.impl;
 
-import static com.github.backend.exception.member.code.MemberErrorCode.MEMBER_NOT_EXISTS;
-
 import com.github.backend.exception.member.MemberException;
-import com.github.backend.persist.member.Member;
+import com.github.backend.exception.member.code.MemberErrorCode;
 import com.github.backend.persist.member.repository.MemberRepository;
+import com.github.backend.persist.member.type.MemberStatus;
+import com.github.backend.persist.member.Member;
+import com.github.backend.persist.common.repository.RefreshTokenRepository;
 import com.github.backend.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
 @RequiredArgsConstructor
-
+@Service
 public class MemberServiceImpl implements MemberService {
 
-    final MemberRepository memberRepository;
+	private final RefreshTokenRepository refreshTokenRepository;
+	private final MemberRepository memberRepository;
 
-    @Transactional(readOnly = true)
-    @Override
-    public Member getMember(String email) {
-        return memberRepository.findByEmail(email)
-            .orElseThrow(()-> new MemberException(MEMBER_NOT_EXISTS));
-    }
+	@Transactional
+	@Override
+	public void editNickname(String memberEmail, String newNickname) {
+		Member member = memberRepository.findByEmail(memberEmail)
+			.orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_EXISTS));
+		member.changeNickname(newNickname);
+	}
 
-    @Transactional(readOnly = true)
-    @Override
-    public Member getMember(Long memberId) {
-        return memberRepository.findById(memberId)
-            .orElseThrow(()->new MemberException(MEMBER_NOT_EXISTS));
-    }
+	@Transactional
+	@Override
+	public void withdrawal(String memberEmail) {
+		Member member = memberRepository.findByEmail(memberEmail)
+			.orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_EXISTS));
+
+		member.changeStatus(MemberStatus.WITHDRAWAL);
+		refreshTokenRepository.deleteByUsername(memberEmail);
+	}
 
 }
