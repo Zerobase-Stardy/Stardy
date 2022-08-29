d374cea2f77f362be7fcb7f82727f3f21e097731
ny <dkzndk1100@naver.com>
2022-08-29 17:37:50 +0900

    [feat] 비회원 강의 검색 (#115)
    what : 비회원 강의 검색 기능 추가
    why : 비회원 강의 검색 기능 추가

--- Backend/src/main/java/com/github/backend/service/course/impl/CourseService.java
+++ Backend/src/main/java/com/github/backend/service/course/impl/CourseService.java
@@ -1,23 +1,32 @@
 package com.github.backend.service.course.impl;
 
+import com.github.backend.dto.course.CourseInfoOutputDto;
+import com.github.backend.dto.course.SearchCourse;
 import com.github.backend.exception.course.CourseException;
 import com.github.backend.persist.course.Course;
 import com.github.backend.persist.course.repository.CourseRepository;
 import com.github.backend.exception.course.code.CourseErrorCode;
+import com.github.backend.persist.course.repository.querydsl.CourseSearchRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class CourseService {
-    private final CourseRepository courseRepository;
+    private final CourseSearchRepository courseSearchRepository;
+
 
     @Transactional
-    public Course getCourseInfo(Long courseId){
+    public List<CourseInfoOutputDto.Info> searchCourseList(SearchCourse searchCourse){
 
-        return courseRepository.findById(courseId)
-                .orElseThrow(() -> new CourseException(CourseErrorCode.NOT_EXIST_COURSE));
+        return courseSearchRepository.searchByWhere(searchCourse.toCondition())
+                .stream()
+                .map(CourseInfoOutputDto.Info::of)
+                .collect(Collectors.toList());
     }
 
 }
--- Backend/src/main/java/com/github/backend/web/course/controller/CourseController.java
+++ Backend/src/main/java/com/github/backend/web/course/controller/CourseController.java
@@ -1,6 +1,8 @@
 package com.github.backend.web.course.controller;
 
 import com.github.backend.dto.common.Result;
+import com.github.backend.dto.course.CourseInfoOutputDto;
+import com.github.backend.dto.course.SearchCourse;
 import com.github.backend.service.course.impl.CourseService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/course")
@@ -17,19 +21,18 @@ public class CourseController {
 
     private final CourseService courseService;
 
-    @GetMapping("/{courseId}")
-    public ResponseEntity<Result<?>> getCourseInfo(
-            @PathVariable("courseId") @Valid Long courseId
-    ){
-        return ResponseEntity.ok()
-                .body(
-                        Result.builder()
-                                .status(200)
-                                .success(true)
-                                .data(courseService.getCourseInfo(courseId))
-                                .build()
-                );
+    @GetMapping("/courses")
+    public ResponseEntity<Result<?>> getCourseList(SearchCourse searchCourse){
 
+        List<CourseInfoOutputDto.Info> courseList = courseService.searchCourseList(searchCourse);
+
+        return ResponseEntity.ok().body(
+                Result.builder()
+                        .status(200)
+                        .success(true)
+                        .data(courseList)
+                        .build()
+        );
     }
 
 
