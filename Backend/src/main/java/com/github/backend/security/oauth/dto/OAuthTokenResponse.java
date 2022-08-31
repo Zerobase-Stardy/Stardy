43b645f26f1fd798b821c9ef3fdbdfe50cb9997c
Dohun Lee <nestour95@gmail.com>
2022-08-31 13:14:38 +0900

    [fix] 회원 인증 및 로그인 로직 변경
    what : 회원 인증 및 로그인을 직접 처리하는 로직을 작성했습니다.

---
+++ Backend/src/main/java/com/github/backend/security/oauth/dto/OAuthTokenResponse.java
@@ -0,0 +1,20 @@
+package com.github.backend.security.oauth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class OAuthTokenResponse {
+
+	private String token_type;
+	private String access_token;
+	private int expires_in;
+	private String refresh_token;
+	private int refresh_token_expires_in;
+
+}
