97e5323e01bdc3ea3263a750d96a4da40c20c5bd
hyunebin <56738768+hyunebin@users.noreply.github.com>
2022-09-01 02:10:10 +0900

    [fix] 게시판 request 수정 (#134)
    [fix] 게시판 request 수정
    what : 게시판 request 수정
    why : RequestPart는 multipart 형태로 들어와 json 데이터를 읽지 못함
    RequestBody형태로 받아와야한다.

--- Backend/src/main/java/com/github/backend/web/post/controller/PostController.java
+++ Backend/src/main/java/com/github/backend/web/post/controller/PostController.java
@@ -7,6 +7,7 @@ import com.github.backend.service.post.PostService;
 import com.github.backend.service.post.impl.S3UploadService;
 import com.github.backend.web.post.dto.PostReq;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -58,7 +59,7 @@ public class PostController {
     @PutMapping("/posts/{postId}")
     public ResponseEntity<Result<?>> updatePost(
             @PathVariable("postId") Long postId
-            , @RequestPart @Valid PostReq.Request request
+            ,@RequestBody @Valid PostReq.Request request
             ,@AuthenticationPrincipal MemberInfo memberInfo
     ) throws IOException {
 
@@ -122,7 +123,7 @@ public class PostController {
     @PostMapping("/posts")
     public ResponseEntity<Result<?>> registerPost(
             @AuthenticationPrincipal MemberInfo memberInfo,
-            @RequestPart @Valid PostReq.Request request
+            @RequestBody @Valid PostReq.Request request
     ) throws IOException {
         PostRegisterOutPutDto.Info postRegisterOutPutDto = postService.registerPost(
                 request,
