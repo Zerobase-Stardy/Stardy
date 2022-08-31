f6c07d8c54a7dc6c8dcd87cda920a4c0277af1cc
Dohun Lee <nestour95@gmail.com>
2022-08-31 13:14:38 +0900

    [fix] 회원 인증 및 로그인 로직 변경
    what : 회원 인증 및 로그인을 직접 처리하는 로직을 작성했습니다.

--- Backend/src/main/java/com/github/backend/exception/common/code/OAuthErrorCode.java
+++ Backend/src/main/java/com/github/backend/exception/common/code/OAuthErrorCode.java
@@ -8,8 +8,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public enum OAuthErrorCode {
-	EMAIL_NOT_FOUND("OAuth2 Provider에게서 이메일을 찾을 수 없습니다."),
-	EMAIL_ALREADY_SIGNED_UP("이미 가입되어있는 이메일입니다.");
+	EMAIL_NOT_FOUND("이메일을 찾을 수 없습니다."),
+	EMAIL_ALREADY_SIGNED_UP("이미 가입되어있는 이메일입니다."),
+	INVALID_PROVIDER("지원하지 않는 OAuth Provider 입니다.");
 	private String description;
 
 	public OAuthErrorCode setDescription(String message) {
