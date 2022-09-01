58b2d907fca6260dace2c17f23a3f48cd2dfb0e4
Dohun Lee <nestour95@gmail.com>
2022-09-02 01:10:04 +0900

    [feat] 내 강의 조회 API (#139)
    * [feat] 내 강의 조회 API
    
    what : 내 강의 조회 API를 작성했습니다.
    why : 구매한 내 강의를 조회할 수 있는 기능을 작성했습니다.
    
    * [feat] Swagger 설명 추가
    
    * [test] 내 강의 상세조회 테스트 작성

--- Backend/src/main/java/com/github/backend/service/course/impl/CourseService.java
+++ Backend/src/main/java/com/github/backend/service/course/impl/CourseService.java
@@ -3,10 +3,14 @@ package com.github.backend.service.course.impl;
 import com.github.backend.dto.course.CourseInfoOutputDto;
 import com.github.backend.dto.course.SearchCourse;
 import com.github.backend.exception.course.CourseException;
+import com.github.backend.exception.myCourse.MyCourseException;
+import com.github.backend.exception.myCourse.code.MyCourseErrorCode;
 import com.github.backend.persist.course.Course;
 import com.github.backend.persist.course.repository.CourseRepository;
 import com.github.backend.exception.course.code.CourseErrorCode;
 import com.github.backend.persist.course.repository.querydsl.CourseSearchRepository;
+import com.github.backend.persist.myCourse.MyCourse;
+import com.github.backend.persist.myCourse.repository.MyCourseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,14 +23,23 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class CourseService {
+
     private final CourseSearchRepository courseSearchRepository;
+    private final MyCourseRepository myCourseRepository;
 
-
-    @Transactional
-    public Page<CourseInfoOutputDto.Info> searchCourseList(SearchCourse searchCourse, Pageable pageable){
+    @Transactional(readOnly = true)
+    public Page<CourseInfoOutputDto.Info> searchCourseList(SearchCourse searchCourse,
+        Pageable pageable) {
 
         return courseSearchRepository.searchByWhere(searchCourse.toCondition(), pageable)
-                .map(CourseInfoOutputDto.Info::of);
+            .map(CourseInfoOutputDto.Info::of);
     }
 
+    @Transactional(readOnly = true)
+    public CourseInfoOutputDto.Info searchMyCourse(Long memberId, Long courseId) {
+        MyCourse myCourse = myCourseRepository.findByMember_IdAndCourse_Id(memberId, courseId)
+            .orElseThrow(() -> new MyCourseException(MyCourseErrorCode.MY_COURSE_NOT_EXISTS));
+
+        return CourseInfoOutputDto.Info.of(myCourse.getCourse());
+    }
 }
