4769a55d7f03107223aca157cad670aaef1defea
Dohun Lee <nestour95@gmail.com>
2022-09-05 21:26:01 +0900

    [hot-fix] 자바 타임존 서울로 설정

--- Backend/src/main/java/com/github/backend/BackendApplication.java
+++ Backend/src/main/java/com/github/backend/BackendApplication.java
@@ -1,11 +1,18 @@
 package com.github.backend;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
 
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
