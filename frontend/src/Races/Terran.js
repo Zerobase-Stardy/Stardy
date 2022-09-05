913092bf33482b6d38e09f2165341d317882470f
Lee-SeungHoon <vheh0901@gmail.com>
2022-09-05 20:55:11 +0900

    [fix]: 비로그인시 모달로

--- frontend/src/Races/Terran.js
+++ frontend/src/Races/Terran.js
@@ -30,28 +30,29 @@ export default function Terran(){
 
     function handleClick(e) {
       const lectureId  = e.currentTarget.id
-      if(login === false) if(login === false) {
+
+      if(login === false) {
         alert("로그인이 필요합니다.");
         dispatch(modal(true));
         e.preventDefault();
-       }
-      axios.post(`https://www.dokuny.blog/courses/${lectureId}/unlock`,{}, {
+       } else {
+         axios.post(`https://www.dokuny.blog/courses/${lectureId}/unlock`,{}, {
            headers: header
-       }).
-       then(()=>{
-        window.alert(`강의가 해금되었습니다! 마이페이지에서 확인 할 수 있습니다`)
-
-      }).
-      catch((err) => {
-        if(err.response.status === 500){
-          window.alert("이미 소지한 강의입니다 마이페이지에서 확인해주세요")
-        } else if (err.response.status === 401){
-          window.alert("먼저 로그인을 해주세요")
-        } else if(err.response.status === 403){
-          window.alert("포인트가 부족합니다")
+          }).
+          then(()=>{
+            window.alert(`강의가 해금되었습니다! 마이페이지에서 확인 할 수 있습니다`)
+            
+          }).
+          catch((err) => {
+            if(err.response.status === 500){
+              window.alert("이미 소지한 강의입니다 마이페이지에서 확인해주세요")
+            } else if(err.response.status === 403){
+              window.alert("포인트가 부족합니다")
+            }
+          })
         }
-      })
-   }
+      }
+ 
  
 
     const levelVideo = lectures.map((data) => { 
