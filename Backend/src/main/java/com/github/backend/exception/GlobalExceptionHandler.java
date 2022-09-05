2298f2103e6393200c043df737bfe2c0192ee83b
Hyunebin <qortjr83@naver.com>
2022-09-05 21:03:23 +0900

    [feat] GlobalExceptionHandler추가
    [feat] GlobalExceptionHandler추가
    what : 핸들러 추가
    why : 핸들러 관리

--- Backend/src/main/java/com/github/backend/exception/GlobalExceptionHandler.java
+++ Backend/src/main/java/com/github/backend/exception/GlobalExceptionHandler.java
@@ -1,8 +1,31 @@
 package com.github.backend.exception;
 
+import com.github.backend.dto.common.ErrorResult;
+import com.github.backend.exception.common.OAuthException;
+import com.github.backend.exception.post.PostException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(value = PostException.class)
+    public ResponseEntity<ErrorResult> postErrorhandler(PostException e){
+        return ResponseEntity.badRequest().body(
+                ErrorResult.builder()
+                        .errorCode(e.getErrorCode().name())
+                        .errorDescription(e.getErrorMessage())
+                        .build()
+        );
+    }
 
+    @ExceptionHandler(value = OAuthException.class)
+    public ResponseEntity<ErrorResult> OAuthErrorhandler(OAuthException e){
+        return ResponseEntity.badRequest().body(
+                ErrorResult.builder()
+                        .errorCode(e.getErrorCode().name())
+                        .errorDescription(e.getErrorMessage())
+                        .build()
+        );
+    }
 }
